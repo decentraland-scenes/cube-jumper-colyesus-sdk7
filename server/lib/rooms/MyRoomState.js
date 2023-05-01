d7e7e46da8798fd850ae305257ccd5a9ecc63b6c
William Caine <william.caineiii@gmail.com>
2023-05-01 14:08:31 -0400

    INITIAL COMMIT

---
+++ server/lib/rooms/MyRoomState.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyRoomState = exports.Block = exports.Player = void 0;
+const schema_1 = require("@colyseus/schema");
+class Player extends schema_1.Schema {
+}
+__decorate([
+    (0, schema_1.type)("string")
+], Player.prototype, "name", void 0);
+__decorate([
+    (0, schema_1.type)("number")
+], Player.prototype, "ranking", void 0);
+exports.Player = Player;
+class Block extends schema_1.Schema {
+}
+__decorate([
+    (0, schema_1.type)("number")
+], Block.prototype, "x", void 0);
+__decorate([
+    (0, schema_1.type)("number")
+], Block.prototype, "y", void 0);
+__decorate([
+    (0, schema_1.type)("number")
+], Block.prototype, "z", void 0);
+exports.Block = Block;
+class MyRoomState extends schema_1.Schema {
+    constructor() {
+        super(...arguments);
+        this.blocks = new schema_1.ArraySchema();
+        this.players = new schema_1.MapSchema();
+    }
+}
+__decorate([
+    (0, schema_1.type)("number")
+], MyRoomState.prototype, "countdown", void 0);
+__decorate([
+    (0, schema_1.type)([Block])
+], MyRoomState.prototype, "blocks", void 0);
+__decorate([
+    (0, schema_1.type)({ map: Player })
+], MyRoomState.prototype, "players", void 0);
+exports.MyRoomState = MyRoomState;
---
+++ src/game.ts
@@ -0,0 +1,5 @@
+// Import Decentraland Builder code.
+import "./scene";
+
+// Import the custom gameplay code.
+import "./gameplay";
